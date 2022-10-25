f036595eda2275b97ffd291107d732d42ad35393
Davide <DaviR87@users.noreply.github.com>
2022-10-25 13:01:22 +0200

    Added reactive validator

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+
+export function regexpValidator(regex: RegExp): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } => {
+    const testPassed: boolean = regex.test(control.value);
+    return testPassed ? null : { 'regExpInvalid': { value: control.value } }
+  }
+}
 
 @Component({
   selector: 'app-reactive',
@@ -10,11 +17,13 @@ export class ReactiveComponent implements OnInit {
 
   loginForm: FormGroup;
   usernameMinLength: number = 5;
+  // Only numbers RegExp
+  public miaRegularExp: RegExp = new RegExp(/^\d+$/)
 
   constructor() {
     this.loginForm = new FormGroup({
       username: new FormControl('', [Validators.required, Validators.minLength(this.usernameMinLength)]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+      password: new FormControl('', [Validators.required, Validators.minLength(8), regexpValidator(this.miaRegularExp)])
     })
   }
 
