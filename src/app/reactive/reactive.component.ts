af9dac0c469e68985315eea6553b6506610150b6
Davide <DaviR87@users.noreply.github.com>
2022-10-25 15:27:26 +0200

    Added formarray and cross validator

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+
+export interface LoginForm {
+  username: FormControl<string>,
+  password: FormControl<string>,
+  start: FormControl<string>,
+  end: FormControl<string>,
+}
 
 export function regexpValidator(regex: RegExp): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } => {
     const testPassed: boolean = regex.test(control.value);
     return testPassed ? null : { 'regExpInvalid': { value: control.value } }
   }
+}
+
+export const dateValidator: ValidatorFn = (fg: FormGroup<LoginForm>) => {
+  const startDate: Date = new Date(fg.controls.start.value);
+  const endDate: Date = new Date(fg.get('end').value);
+  return startDate !== null && endDate !== null && startDate < endDate ? null : { dateRangeInvalid: true }
 }
 
 @Component({
@@ -23,14 +36,37 @@ export class ReactiveComponent implements OnInit {
   constructor() {
     this.loginForm = new FormGroup({
       username: new FormControl('', [Validators.required, Validators.minLength(this.usernameMinLength)]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8), regexpValidator(this.miaRegularExp)])
-    })
+      password: new FormControl('', [Validators.required, Validators.minLength(8), regexpValidator(this.miaRegularExp)]),
+      start: new FormControl(),
+      end: new FormControl(),
+      isAdmin: new FormControl(false),
+      streets: new FormArray([])
+    }, dateValidator );
+
+    this.loginForm.get('isAdmin').valueChanges.subscribe((val: boolean) => {
+      console.log(val);
+      if (val) {
+        this.loginForm.get('password').clearValidators();
+        this.loginForm.get('password').addValidators([Validators.required, Validators.minLength(8)]);
+        this.loginForm.get('password').updateValueAndValidity();
+      } else {
+        // this.loginForm.get('password').clearValidators();
+        this.loginForm.get('password').addValidators([regexpValidator(this.miaRegularExp)]);
+        this.loginForm.get('password').updateValueAndValidity();
+      }
+    });
   }
 
   ngOnInit(): void {
+
   }
 
-  gestisciDati() {
+  addStreet() {
+    const streetsFA: FormArray = this.loginForm.get('streets') as FormArray;
+    streetsFA.push(new FormControl('', Validators.required));
+  }
+
+  gestisciDati(data?: FormGroup<LoginForm>) {
     console.log(this.loginForm);
   }
 
